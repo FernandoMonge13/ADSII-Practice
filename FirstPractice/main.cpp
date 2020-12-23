84e2c5780ad056d92a1a1cfdd6b5201c44d331f3
Fernando <fercoche369@gmail.com>
2020-12-23 15:24:51 -0600

    First Commit

---
+++ FirstPractice/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h> // Libreria de mate
+using namespace std;
+
+int main() {
+    int num,a ;
+
+    cout << "Deme un numero: ";
+    cin >> num;
+    cout << "El numero es:" << num;
+    a = sqrt(num); // Raiz
+    a = pow(num, 2); // Elevacion
+
+
+    return 0;
+}
---
+++ cmake-build-debug/CMakeFiles/Practica_C__.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Practica_C__.dir/FirstPractice/main.cpp.obj"
+  "Practica_C__.exe"
+  "Practica_C__.exe.manifest"
+  "Practica_C__.pdb"
+  "libPractica_C__.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Practica_C__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
